8dfe3a18ded0b32e0c7bbcbe277589f7596ed38e
watcha.ac@gmail.com <watcha.ac@gmail.com>
2016-07-09 21:59:02 +0700

    improve

--- netforce_inspect/__init__.py
+++ netforce_inspect/__init__.py
@@ -1,2 +1,3 @@
 from . import models
 from . import migrations
+from . import controllers
---
+++ netforce_inspect/controllers/checkqr.py
@@ -0,0 +1,34 @@
+from netforce.controller import Controller
+from netforce.template import render
+#from netforce import config
+from netforce.database import get_connection
+#from netforce import access
+#from netforce import config
+from netforce.model import get_model
+
+class CheckQR(Controller):
+    _path="/inspectionreport/checkqr"
+
+    def get(self):
+        #url="https://v5.inspection.in.th/inspectionreport/checkqr?id=1541844&password=f45681f1"
+        db=get_connection() # prevent to error get transaction
+        try:
+            db.begin()
+            id=self.get_argument("id")
+            password=self.get_argument("password")
+            data={
+                'id': id,
+                'password': password,
+            }
+            res=get_model("car.check").search_read([])
+            if res:
+                data['obj']=res[-1]
+            html=render("checkqr",data)
+            self.write(html)
+        except Exception as e:
+            self.write("ERROR : %s"%str(e))
+        finally:
+            if db:
+                db.commit()
+
+CheckQR.register()
