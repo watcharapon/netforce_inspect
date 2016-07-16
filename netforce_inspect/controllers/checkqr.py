067142f2ecc7130fe85895c8c9fa82747785b3e8
watcha.ac@gmail.com <watcha.ac@gmail.com>
2016-07-16 08:47:06 +0700

    improve

--- netforce_inspect/controllers/checkqr.py
+++ netforce_inspect/controllers/checkqr.py
@@ -3,6 +3,7 @@ from netforce.template import render
 from netforce.locale import set_active_locale, get_active_locale
 from netforce.database import get_connection
 from netforce.model import get_model
+from netforce.access import set_active_user
 
 class CheckQR(Controller):
     _path="/inspectionreport/checkqr"
@@ -10,6 +11,7 @@ class CheckQR(Controller):
     def get(self):
         #url="https://v5.inspection.in.th/inspectionreport/checkqr?id=1541844&password=f45681f1"
         current_local=get_active_locale()
+        set_active_user(1) #admin
         set_active_locale('th_TH')
         db=get_connection() # prevent to error get transaction
         try:
@@ -25,7 +27,12 @@ class CheckQR(Controller):
             ctx={
                 'obj': None,
             }
-            res=db.query("select * from inspection where number=%s and password=%s",id,password)
+            #res=db.query("select * from inspection where number=%s and password=%s",id,password)
+            dom=[
+                ['number','=', id],
+                ['password','=', password]
+            ]
+            res=get_model("inspection").search_read(dom)
             if res:
                 ctx['obj']=res[-1]
             else:
